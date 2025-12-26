a291699fc9fd0a7acc5dce0745bfd719195efdb5
Tarun Kumar <tarunkumar291103@gmail.com>
2025-12-26 21:08:01 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,78 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var gameStarted = false;
+var level = 0;
+
+
+$(document).keydown(function(){
+    if(!gameStarted){
+        nextSequence();
+        gameStarted = true;
+    }
+});
+
+
+$(".btn").on("click", function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log("success");
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 800);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+function nextSequence(){
+    userClickedPattern = [];
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber]; 
+    gamePattern.push(randomChosenColour);
+    
+    level++;
+    $("#level-title").text("Level "+level);
+    
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+
+function startOver(){
+    gameStarted = false;
+    level = 0;
+    gamePattern = []; 
+}
+
+
+function playSound(name){
+    var audio = new Audio("./sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    } , 100);
+}
